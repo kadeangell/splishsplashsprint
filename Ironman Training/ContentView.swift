8355ec50da93c6082a3f07aca4a53ecc4d3696ee
Kade Angell <kade@angell.fyi>
2025-10-21 20:00:59 -0600

    Initial Commit

---
+++ Ironman Training/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ironman Training
+//
+//  Created by Kade Angell on 10/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ironman Training/Ironman_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ironman_TrainingApp.swift
+//  Ironman Training
+//
+//  Created by Kade Angell on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Ironman_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
